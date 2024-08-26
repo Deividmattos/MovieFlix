7355ab5a6aa0a7dca9d6ee805775dd7b43fd0876
devDeivid <deividmattos97@gmail.com>
2024-08-26 19:21:46 +0100

    create css filme

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 
+import Erro from "./pages/erro";
+
 import Header from "./components/Header";
 
 function RoutesApp() {
   return (
     <BrowserRouter>
       <Header />
-
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/filme/:id" element={<filme />} />
+        <Route path="/filme/:id" element={<Filme />} />
+
+        <Route path="*" element={<Erro />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default RoutesApp;

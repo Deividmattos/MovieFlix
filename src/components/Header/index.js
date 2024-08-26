a1a7a61b328efdc4c99b6e2cef7d375cd6151f56
devDeivid <deividmattos97@gmail.com>
2024-08-26 19:35:23 +0100

    Create button back for home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,17 @@
-import './header.css';
-import { Link } from 'react-router-dom'
+import "./header.css";
+import { Link } from "react-router-dom";
 
-function Header(){
-  return(
+function Header() {
+  return (
     <header>
-      <Link className="logo" to="/">Prime Flix</Link>
-      <Link className="favoritos" to="/favoritos">Meus filmes</Link>
+      <Link className="logo" to="/">
+        Movie Flix
+      </Link>
+      <Link className="favoritos" to="/favoritos">
+        Meus filmes
+      </Link>
     </header>
-  )
+  );
 }
 
 export default Header;

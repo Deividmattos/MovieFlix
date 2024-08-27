674691d2c19cf1c5eb68cd02dbb6c2bf9a034d64
devDeivid <deividmattos97@gmail.com>
2024-08-27 17:12:54 +0100

    Excluir filme

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,18 +1,33 @@
 import { useEffect, useState } from "react";
 import "./favoritos.css";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Favoritos() {
   const [filmes, setFilmes] = useState([]);
 
   useEffect(() => {
-    const minhaLista = localStorage.getItem("@movieflix");
+    const minhaLista = localStorage.getItem("@primeflix");
     setFilmes(JSON.parse(minhaLista) || []);
   }, []);
+
+  function excluirFilme(id) {
+    let filtroFilmes = filmes.filter((item) => {
+      return item.id !== id;
+    });
+
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+    toast.success("Filme removido com sucesso");
+  }
 
   return (
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
+
+      {filmes.length === 0 && (
+        <span>Você não possui nenhum filme salvo :( </span>
+      )}
 
       <ul>
         {filmes.map((item) => {
@@ -22,7 +37,7 @@ function Favoritos() {
 
               <div>
                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
               </div>
             </li>
           );
